2a9074f69e7f17548249169c15e335d146269944
Vlad Ki <vlad@kirillov.im>
2020-06-30 13:02:41 +0300

    support multiple backends by specifying a backend_ref in config

--- include/mod_pushoff.hrl
+++ include/mod_pushoff.hrl
@@ -1,6 +1,6 @@
 -type bare_jid() :: {binary(), binary()}.
--type backend_type() :: apns | fcm.
--type backend_id() :: {binary(), backend_type()}.
+-type backend_ref() :: apns | fcm | {apns, binary()} | {fcm, binary()}.
+-type backend_id() :: {binary(), backend_ref()}.
 
 -record(pushoff_registration, {bare_jid :: bare_jid(),
                                token :: binary(),
